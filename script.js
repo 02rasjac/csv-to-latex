157ca56633f8925b12e28a58754bd9488563ee04
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-03-04 11:42:54 +0100

    Reading CSV and generating Latex-table
    TODO before fully working:
    - Add generation of column-position (i.e \begin{tabular}{cc}).

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ let input = document.querySelector('#csv > textarea');
 input.textContent = `a,b
 1,2
 3,4`;
+let output = document.querySelector('#latex code');
 
 console.log(input.textContent);
 
 function ReadCSV() {
-  let test = input.textContent.split('\n');
+  let test = input.textContent.split('\n').map((x) => x.split(','));
   console.log(test);
+
+  for (let i = 0; i < test.length; i++) {
+    let row = '        ';
+    for (let j = 0; j < test[i].length; j++) {
+      row += test[i][j];
+      if (j < test[i].length - 1) {
+        row += ' & ';
+      }
+    }
+    row += ' \\\\\n';
+    output.textContent += row;
+  }
 }
 
+output.textContent = `\\begin{table}[!ht]
+    \\begin{tabular}
+`;
 ReadCSV();
+output.textContent += `    \\end{tabular}
+\\end{table}
+`;
